b94ea4ab3c627e6c22386de6672ff4a72cb927fb
adriano.souza <adriano.souza@zpesystems.com>
2022-03-13 11:41:17 -0300

    Listing repositories on component

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -4,14 +4,6 @@ import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 
 
-//https://api.github.com/orgs/rocketseat/repos
-
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/unform/unform'
-}
-
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([])
 
@@ -26,10 +18,9 @@ export function RepositoryList() {
       <h1>Repository List</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />
+        })}
       </ul>
     </section>
   );
