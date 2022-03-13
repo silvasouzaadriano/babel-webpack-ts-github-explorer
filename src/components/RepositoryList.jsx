a5c947aab8847c6acaa63281deac908a0551e69e
adriano.souza <adriano.souza@zpesystems.com>
2022-03-13 10:31:32 -0300

    Using properties

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,30 +1,21 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+  name: 'unform',
+  description: 'Forms in React',
+  link: 'https://github.com/unform/unform'
+}
+
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Repository List</h1>
 
       <ul>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-          <a href="">
-            Access repository
-          </a>
-        </li>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-          <a href="">
-            Access repository
-          </a>
-        </li>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-          <a href="">
-            Access repository
-          </a>
-        </li>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository}/>
+        <RepositoryItem repository={repository}/>
+        <RepositoryItem repository={repository}/>
       </ul>
     </section>
   );
