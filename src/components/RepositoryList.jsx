0e07ae83af542b8654811dd58eaf7a8b4f8e9409
adriano.souza <adriano.souza@zpesystems.com>
2022-03-13 10:17:33 -0300

    First component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,31 @@
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Repository List</h1>
+
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">
+            Access repository
+          </a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">
+            Access repository
+          </a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">
+            Access repository
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
+}
